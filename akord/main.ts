85b52eb6252c5ee2724bce6e028ed292bd4bc98a
0x1a1 <>
2023-09-02 23:15:32 +0800

    2023-09-02.1

--- akord/main.ts
+++ akord/main.ts
@@ -1,7 +1,7 @@
 import {Akord, Auth} from "@akord/akord-js";
 
 async function main() {
-    const {wallet} = await Auth.signIn("0x11a1@proton.me", "W5HxPyzi_g2QopdZ46af");
+    const {wallet} = await Auth.signIn("", "");
     const akord = await Akord.init(wallet);
 
     const vaults = await akord.vault.listAll();
